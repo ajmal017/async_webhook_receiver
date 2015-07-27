80f89b81e9531344eb7a9bcdde17f6e1f2381760
surge <sbryukov@gmail.com>
2015-07-27 09:39:59 +0300

    init

---
+++ webhook_callback_async.py
@@ -0,0 +1,54 @@
+import Queue
+import threading
+import time
+from flask import Flask
+
+callback_queue = Queue.Queue()
+
+def from_dummy_thread(func_to_call_from_main_thread):
+    callback_queue.put(func_to_call_from_main_thread)
+
+def from_flask_thread(func_to_call_from_main_thread):
+    callback_queue.put(func_to_call_from_main_thread)
+
+def from_main_thread_blocking():
+    callback = callback_queue.get() #blocks until an item is available
+    callback()
+
+def from_main_thread_nonblocking():
+    while True:
+        try:
+            callback = callback_queue.get(False) #doesn't block
+        except Queue.Empty: #raised when queue is empty
+            break
+        callback()
+
+def print_num(dummyid, n):
+    print "From %s: %d" % (dummyid, n)
+
+
+def dummy_run(dummyid):
+    for i in xrange(5):
+        from_dummy_thread(lambda: print_num(dummyid, i))
+        time.sleep(0.5)
+
+def flask_webhook(thid):
+	app = Flask(__name__)
+
+
+	@app.route('/message')
+	def hello_world():
+		print 'Flask got message'
+		from_flask_thread(lambda: print_num(thid, 12))
+	
+
+	app.run()
+
+	
+
+
+threading.Thread(target=dummy_run, args=("a",)).start()
+threading.Thread(target=flask_webhook, args=("Flask",)).start()
+
+while True:
+    from_main_thread_blocking()
